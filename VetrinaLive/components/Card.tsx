026fa996ed2683c1a9210bf9aabcb93e34368740
Nemanja Dobranic <dobranicnemanja2014@gmail.com@>
2022-12-11 19:21:18 +0100

    custom card component

---
+++ VetrinaLive/components/Card.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {colors, spacing} from '../theme/main';
+import LinearGradient from 'react-native-linear-gradient';
+
+type StyleProps = {
+  backgroundColor?: string;
+  height?: number;
+  elevation?: number;
+  marginTop?: number;
+  marginBottom?: number;
+  marginStart?: number;
+  marginEnd?: number;
+  borderRadius?: number;
+};
+
+type Props = StyleProps & {
+  linearGradient?: string[];
+  children?: React.ReactNode | React.ReactNode[];
+};
+
+const Card: React.FC<Props> = ({children, linearGradient, ...styleProps}) => {
+  const {root} = styles(styleProps);
+  return (
+    <View style={root}>
+      {linearGradient ? (
+        <LinearGradient
+          colors={linearGradient}
+          style={{minHeight: styleProps.height}}>
+          {React.Children.toArray(children) ?? <></>}
+        </LinearGradient>
+      ) : (
+        React.Children.toArray(children) ?? <></>
+      )}
+    </View>
+  );
+};
+
+const styles = ({
+  backgroundColor,
+  height,
+  elevation,
+  marginTop,
+  marginBottom,
+  marginEnd,
+  marginStart,
+  borderRadius,
+}: StyleProps) =>
+  StyleSheet.create({
+    root: {
+      backgroundColor: backgroundColor ?? colors.white,
+      height: height,
+      shadowColor: colors.grey[700],
+      elevation: elevation,
+      marginTop: marginTop ?? 1.5 * spacing.s,
+      marginBottom: marginBottom ?? 1.5 * spacing.s,
+      marginStart: marginStart ?? 1.8125 * spacing.s,
+      marginEnd: marginEnd ?? 1.8125 * spacing.s,
+      borderRadius: borderRadius ?? 1.25 * spacing.s,
+    },
+  });
+
+export default Card;
--- VetrinaLive/screens/vetrinaLive/Dashboard.tsx
+++ VetrinaLive/screens/vetrinaLive/Dashboard.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {useSelector} from 'react-redux';
 import {IRootState} from '../../redux/rootReducer';
+import Card from '../../components/Card';
+import {colors, spacing} from '../../theme/main';
+import textVariants from '../../theme/textVariants';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
 const Dashboard = () => {
   const {name} = useSelector((state: IRootState) => state.user);
 
-  return <Text>Dashboard, {name}</Text>;
+  return (
+    <>
+      <Card
+        linearGradient={[colors.primary.default, '#C2E8FA']}
+        marginBottom={0}
+        marginEnd={0}
+        marginStart={0}
+        marginTop={0}
+        height={6 * spacing.xl}>
+        <Text style={{...textVariants.title.title1, color: colors.white}}>
+          Welcome {name}!
+        </Text>
+        <View style={styles.storeLink}>
+          <Text
+            style={{
+              ...textVariants.title.title5,
+              color: colors.white,
+              marginRight: 1.25 * spacing.m,
+            }}>
+            app.vetrinalive.com/{name?.toLocaleLowerCase()}-store
+          </Text>
+          <FeatherIcon
+            name="external-link"
+            color={colors.white}
+            size={spacing.l}
+          />
+        </View>
+      </Card>
+    </>
+  );
 };
+
+const styles = StyleSheet.create({
+  storeLink: {
+    marginTop: 1.125 * spacing.m,
+    marginLeft: 1.125 * spacing.m,
+    flexDirection: 'row',
+    alignContent: 'center',
+  },
+});
 
 export default Dashboard;
