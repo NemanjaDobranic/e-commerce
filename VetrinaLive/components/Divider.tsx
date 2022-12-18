fcbe6204a3b1e82e40d76dfe2fe408c3442bc6bc
Nemanja Dobranic <dobranicnemanja2014@gmail.com@>
2022-12-18 12:31:41 +0100

    Divider refactoring

--- VetrinaLive/components/Divider.tsx
+++ VetrinaLive/components/Divider.tsx
@@ -3,49 +3,50 @@ import {View, Text, StyleSheet} from 'react-native';
 import {colors, spacing} from '../theme/main';
 import textVariants from '../theme/textVariants';
 
-interface Props {
-  content?: string;
+interface StyleProps {
   marginVertical?: number;
   color?: string;
 }
 
-const Divider: React.FC<Props> = ({content, marginVertical, color}) => {
+interface Props extends StyleProps {
+  content?: string;
+}
+
+const Divider: React.FC<Props> = ({content, ...styleProps}) => {
+  const {root, contentStyle, divider, text} = styles(styleProps);
   return (
-    <View
-      style={{
-        ...styles.root,
-        marginVertical: marginVertical,
-        backgroundColor: color,
-      }}>
-      <View style={styles.divider} />
+    <View style={root}>
+      <View style={divider} />
       {content && (
-        <View style={styles.content}>
-          <Text style={styles.text}>{content}</Text>
+        <View style={contentStyle}>
+          <Text style={text}>{content}</Text>
         </View>
       )}
-      <View style={styles.divider} />
+      <View style={divider} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  root: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  divider: {
-    flex: 1,
-    height: 1,
-    backgroundColor: colors.grey[500],
-  },
-  content: {
-    paddingLeft: spacing.l,
-    paddingRight: spacing.l,
-  },
-  text: {
-    ...textVariants.button.large,
-    color: colors.grey[500],
-  },
-});
+const styles = ({marginVertical, color}: StyleProps) =>
+  StyleSheet.create({
+    root: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginVertical: marginVertical,
+    },
+    divider: {
+      flex: 1,
+      height: 1,
+      backgroundColor: color ?? colors.grey[500],
+    },
+    contentStyle: {
+      paddingLeft: spacing.l,
+      paddingRight: spacing.l,
+    },
+    text: {
+      ...textVariants.button.large,
+      color: colors.grey[500],
+    },
+  });
 
 export default Divider;
